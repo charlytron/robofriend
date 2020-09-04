617a49da25a84d727b62d9eb1d70d21fef5ace8d
C Tompkins <celloviolinbass@yahoo.com>
2020-09-04 14:45:06 -0500

    Updates

---
+++ precache-manifest.66663c28e1f36f41e41c7f6c6925110b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9032162a9c358f6e6c9e9eb5e8547cb5",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8ee73135c8603ef2a6bf",
+    "url": "/robofriend/static/css/main.5bcc1080.chunk.css"
+  },
+  {
+    "revision": "78c7f849462c00a0a520",
+    "url": "/robofriend/static/js/2.be3d66de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.be3d66de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee73135c8603ef2a6bf",
+    "url": "/robofriend/static/js/main.b8fb23e5.chunk.js"
+  },
+  {
+    "revision": "b9549992ed9aa0bec102",
+    "url": "/robofriend/static/js/runtime-main.b099a5de.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
